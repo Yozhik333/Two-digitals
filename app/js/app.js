0aee47b7e1a1665bc9fdbee8bda0a4e1c7591683
Yozhik333 <44803662+Yozhik333@users.noreply.github.com>
2021-09-23 18:07:29 +0300

    last pls

--- app/js/app.js
+++ app/js/app.js
@@ -59,6 +59,23 @@ document.addEventListener('DOMContentLoaded', () => {
 	} else {
 		document.body.classList.add('_pc');
 
+		// Найти все ссылки начинающиеся на #
+		const anchors = document.querySelectorAll('a[href^="#consultation-yak"]')
+
+		// Цикл по всем ссылкам
+		for (let anchor of anchors) {
+			anchor.addEventListener("click", function (e) {
+				e.preventDefault() // Предотвратить стандартное поведение ссылок
+				// Атрибут href у ссылки, если его нет то перейти к body (наверх не плавно)
+				const goto = anchor.hasAttribute('href') ? anchor.getAttribute('href') : 'body'
+				// Плавная прокрутка до элемента с id = href у ссылки
+				document.querySelector(goto).scrollIntoView({
+					behavior: "smooth",
+					block: "start"
+				})
+			})
+		}
+
 	}
 	// IsMobile end
 
