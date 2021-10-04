d668faa0183058a2fcbabe1928edd6c74ea51add
Yozhik333 <44803662+Yozhik333@users.noreply.github.com>
2021-10-04 14:33:40 +0300

    pls go

--- app/js/quiz.js
+++ app/js/quiz.js
@@ -1,238 +1,239 @@
 let quizContent = {
-    "Автоматизированный маркетинг": ["Контекстная реклама",
-        "Yandex Dzen", "Таргетированная реклама", "CRM", "Ведение социальных сетей",
-        "Управление репутацией в интернете", "Продвижение мобильных приложений"
-    ],
+	"Автоматизированный маркетинг": ["Контекстная реклама",
+		"Yandex Dzen", "Таргетированная реклама", "CRM", "Ведение социальных сетей",
+		"Управление репутацией в интернете", "Продвижение мобильных приложений"
+	],
 
-    "Веб-разработка": ["Создание сайтов", "Веб-дизайн", "Брендинг",
-        "Создание мобильных  приложений", "Фирменный стиль", "Нейминг"
-    ],
+	"Веб-разработка": ["Создание сайтов", "Веб-дизайн", "Брендинг",
+		"Создание мобильных  приложений", "Фирменный стиль", "Нейминг"
+	],
 
-    "Дизайн": ["Создание сайтов", "Веб-дизайн", "Брендинг", "Разработка логотипов",
-        "Создание мобильных  приложений", "Фирменный стиль", "Нейминг"
-    ],
+	"Дизайн": ["Создание сайтов", "Веб-дизайн", "Брендинг", "Разработка логотипов",
+		"Создание мобильных  приложений", "Фирменный стиль", "Нейминг"
+	],
 
-    "Обучение": ["Консультация с таргетологом", "Консультация с веб-разработчиком", "Консультация с дизайнером",
-        "Консультация по созданию личного бренда", "Консультация с владельцами диджитал агентства"
-    ],
+	"Обучение": ["Консультация с таргетологом", "Консультация с веб-разработчиком", "Консультация с дизайнером",
+		"Консультация по созданию личного бренда", "Консультация с владельцами диджитал агентства"
+	],
 
-    "Комплексные решения": ["Complex Light (сайт + seo + ведение инстаграма )", "Complex Medium (сайт + seo + контекст + ведение инстаграма + таргет)",
-        "Complex Pro (брендинг + фирм стиль + сайт+контекст + сео + таргет + соцсети + емаил маркетинг)"
-    ],
+	"Комплексные решения": ["Complex Light (сайт + seo + ведение инстаграма )", "Complex Medium (сайт + seo + контекст + ведение инстаграма + таргет)",
+		"Complex Pro (брендинг + фирм стиль + сайт+контекст + сео + таргет + соцсети + емаил маркетинг)"
+	],
 }
 
-class Quiz{
-    constructor(element, i){
-        this.container = element;
-        this.quizIndex = i;
+class Quiz {
+	constructor(element, i) {
+		this.container = element;
+		this.quizIndex = i;
 
-        this.direction = true;
-        this.directionTitle = '';
-        this.selectedQuiz = {}
+		this.direction = true;
+		this.directionTitle = '';
+		this.selectedQuiz = {}
 
-        this.start();
-    }
+		this.start();
+	}
 
-    start(){
+	start() {
 
-        this.container.querySelector(".form").id = "form__call-do"+this.quizIndex;
+		this.container.querySelector(".form").id = "form__call-do" + this.quizIndex;
 
-        this.container.querySelectorAll(".quiz__radio").forEach((item, i)=> {
-            item.querySelector('input').id = "quiz"+ (this.quizIndex) +"-radio-"+(i+1);
-            // item.querySelector('label').for = "quiz-radio-2";
-            item.querySelector('label').setAttribute('for', 'quiz'+ (this.quizIndex) +'-radio-'+(i+1));
-        });
-
-
-
-        this.container.querySelectorAll(".quiz__radio-input").forEach(item => {
-            item.addEventListener("click", ()=>{
-                this.container.querySelector('.quiz__btns-next').classList.remove('disable');
-            })
-        });
-
-        this.container.querySelector('.quiz__btns-next').addEventListener('click', ()=>{
-            this.nextBtn();
-            
-            if(this.direction) {
-                this.container.querySelector('.quiz-step__btn').classList.remove('add-none');
-            }
-        });
-
-        this.container.querySelector('.quiz__btns-back').addEventListener('click', ()=>{
-            this.prevBtn();
-            this.container.querySelector('.quiz-step__form').classList.add('add-none');
-
-            if(this.direction && Object.keys(this.selectedQuiz).length > 0) {
-                this.container.querySelector('.quiz-step__btn').classList.remove('add-none');
-            }
-        });
-
-        this.container.querySelector('.quiz-step__btn').addEventListener('click', ()=>{
-            this.container.querySelector('.quiz-step__radio-list').classList.add('add-none');
-            this.container.querySelector('.quiz-step__checkbox-list').classList.add('add-none');
-
-            this.container.querySelector('.quiz-step__form').classList.remove('add-none');
-
-            //FIXME:  This variable (selectedQuiz) must be sent to the server
-            console.log(this.selectedQuiz) 
-            
-            let data = '';
-            for (let key in this.selectedQuiz) {
-                
-                data += key + ":\t";
-                this.selectedQuiz[key].forEach(item => {
-                    data += item+", ";
-                });
-
-                data += "|" + "\n";
-            }
-            console.log(data);
-
-            this.container.querySelector('#user-select_quiz').value = data;
-            //FIXME:  This variable (selectedQuiz) must be sent to the server
-            
-        });
-    }
-
-    nextBtn(){
-        this.container.querySelector('.quiz__btns-back').classList.remove('disable'); 
-        this.container.querySelector('.quiz__btns-next').classList.toggle('disable');
-
-        if(!this.direction) {
-            this.container.querySelectorAll(".quiz__radio").forEach(item => {
-                if(item.querySelector('input').checked){
-                    item.classList.add('add-none');
-                }
-            });
-
-            let lsContent = [];
-            this.selectedQuiz[this.directionTitle] = lsContent
-            this.container.querySelectorAll(".quiz__checkbox").forEach(item => {
-                if(item.querySelector('input').checked){
-                    lsContent.push(item.querySelector('label').innerText);
-                }
-
-            });
-            
-
-        }
-        this.container.querySelector('.quiz-step__radio-list').classList.toggle('add-none');
-        this.container.querySelector('.quiz-step__checkbox-list').classList.toggle('add-none');
-        if(this.direction){
-            this.prepareСontent();
-        }
-        this.direction = !this.direction;
-        
-    }
-    prevBtn(){
-        if(Object.keys(this.selectedQuiz).length <= 0){
-            this.container.querySelector('.quiz__btns-back').classList.add('disable'); 
-            this.container.querySelector('.quiz-step__btn').classList.add('add-none'); 
-        }
+		this.container.querySelectorAll(".quiz__radio").forEach((item, i) => {
+			item.querySelector('input').id = "quiz" + (this.quizIndex) + "-radio-" + (i + 1);
+			// item.querySelector('label').for = "quiz-radio-2";
+			item.querySelector('label').setAttribute('for', 'quiz' + (this.quizIndex) + '-radio-' + (i + 1));
+		});
 
 
 
-        // this.container.querySelector('.quiz__btns-next').classList.toggle('disable');
+		this.container.querySelectorAll(".quiz__radio-input").forEach(item => {
+			item.addEventListener("click", () => {
+				this.container.querySelector('.quiz__btns-next').classList.remove('disable');
+			})
+		});
 
-        if(!this.direction) {
-            
-            let last = Object.keys(this.selectedQuiz)[Object.keys(this.selectedQuiz).length-1];
-            this.container.querySelectorAll(".quiz__radio").forEach(item => {
-                if(item.querySelector('input').checked){
-                    this.container.querySelector('.quiz__btns-next').classList.remove('disable'); 
-                }
-            });
+		this.container.querySelector('.quiz__btns-next').addEventListener('click', () => {
+			this.nextBtn();
 
-            
-        }
-        this.container.querySelector('.quiz-step__radio-list').classList.toggle('add-none');
-        this.container.querySelector('.quiz-step__checkbox-list').classList.toggle('add-none');
-        if(this.direction){
-            this.preparePrevСontent();
+			if (this.direction) {
+				this.container.querySelector('.quiz-step__btn').classList.remove('add-none');
+			}
+		});
 
-            let last = Object.keys(this.selectedQuiz)[Object.keys(this.selectedQuiz).length-1];
-            this.container.querySelectorAll(".quiz__radio").forEach(item => {
-                if(item.querySelector('input').value == last){
-                    item.classList.remove('add-none');
-                }
-            });
+		this.container.querySelector('.quiz__btns-back').addEventListener('click', () => {
+			this.prevBtn();
+			this.container.querySelector('.quiz-step__form').classList.add('add-none');
 
-            
-            delete this.selectedQuiz[last];
-            
+			if (this.direction && Object.keys(this.selectedQuiz).length > 0) {
+				this.container.querySelector('.quiz-step__btn').classList.remove('add-none');
+			}
+		});
 
-        }
-        this.direction = !this.direction;
-        
-    }
+		this.container.querySelector('.quiz-step__btn').addEventListener('click', () => {
+			this.container.querySelector('.quiz-step__radio-list').classList.add('add-none');
+			this.container.querySelector('.quiz-step__checkbox-list').classList.add('add-none');
+			this.container.querySelector('.quiz-step__btn').classList.add('add-none');
 
-    prepareСontent(){
-        this.container.querySelector('.quiz-step__btn').classList.add('add-none');
+			this.container.querySelector('.quiz-step__form').classList.remove('add-none');
 
-        this.container.querySelectorAll(".quiz__radio-input").forEach(item => {
-            if(item.checked){
-                let content = '';
-                for(let i = 0; i < quizContent[item.value].length; i++){
-                    content += "<div class='quiz__checkbox'> \
-					                <input type='checkbox' id='quiz"+ (this.quizIndex) +"-checkbox-"+(i+1)+"'> \
-					                <label for='quiz"+ (this.quizIndex) +"-checkbox-"+(i+1)+"'>"+quizContent[item.value][i]+"</label> \
+			//FIXME:  This variable (selectedQuiz) must be sent to the server
+			console.log(this.selectedQuiz)
+
+			let data = '';
+			for (let key in this.selectedQuiz) {
+
+				data += key + ":\t";
+				this.selectedQuiz[key].forEach(item => {
+					data += item + ", ";
+				});
+
+				data += "|" + "\n";
+			}
+			console.log(data);
+
+			this.container.querySelector('#user-select_quiz').value = data;
+			//FIXME:  This variable (selectedQuiz) must be sent to the server
+
+		});
+	}
+
+	nextBtn() {
+		this.container.querySelector('.quiz__btns-back').classList.remove('disable');
+		this.container.querySelector('.quiz__btns-next').classList.toggle('disable');
+
+		if (!this.direction) {
+			this.container.querySelectorAll(".quiz__radio").forEach(item => {
+				if (item.querySelector('input').checked) {
+					item.classList.add('add-none');
+				}
+			});
+
+			let lsContent = [];
+			this.selectedQuiz[this.directionTitle] = lsContent
+			this.container.querySelectorAll(".quiz__checkbox").forEach(item => {
+				if (item.querySelector('input').checked) {
+					lsContent.push(item.querySelector('label').innerText);
+				}
+
+			});
+
+
+		}
+		this.container.querySelector('.quiz-step__radio-list').classList.toggle('add-none');
+		this.container.querySelector('.quiz-step__checkbox-list').classList.toggle('add-none');
+		if (this.direction) {
+			this.prepareСontent();
+		}
+		this.direction = !this.direction;
+
+	}
+	prevBtn() {
+		if (Object.keys(this.selectedQuiz).length <= 0) {
+			this.container.querySelector('.quiz__btns-back').classList.add('disable');
+			this.container.querySelector('.quiz-step__btn').classList.add('add-none');
+		}
+
+
+
+		// this.container.querySelector('.quiz__btns-next').classList.toggle('disable');
+
+		if (!this.direction) {
+
+			let last = Object.keys(this.selectedQuiz)[Object.keys(this.selectedQuiz).length - 1];
+			this.container.querySelectorAll(".quiz__radio").forEach(item => {
+				if (item.querySelector('input').checked) {
+					this.container.querySelector('.quiz__btns-next').classList.remove('disable');
+				}
+			});
+
+
+		}
+		this.container.querySelector('.quiz-step__radio-list').classList.toggle('add-none');
+		this.container.querySelector('.quiz-step__checkbox-list').classList.toggle('add-none');
+		if (this.direction) {
+			this.preparePrevСontent();
+
+			let last = Object.keys(this.selectedQuiz)[Object.keys(this.selectedQuiz).length - 1];
+			this.container.querySelectorAll(".quiz__radio").forEach(item => {
+				if (item.querySelector('input').value == last) {
+					item.classList.remove('add-none');
+				}
+			});
+
+
+			delete this.selectedQuiz[last];
+
+
+		}
+		this.direction = !this.direction;
+
+	}
+
+	prepareСontent() {
+		this.container.querySelector('.quiz-step__btn').classList.add('add-none');
+
+		this.container.querySelectorAll(".quiz__radio-input").forEach(item => {
+			if (item.checked) {
+				let content = '';
+				for (let i = 0; i < quizContent[item.value].length; i++) {
+					content += "<div class='quiz__checkbox'> \
+					                <input type='checkbox' id='quiz"+ (this.quizIndex) + "-checkbox-" + (i + 1) + "'> \
+					                <label for='quiz"+ (this.quizIndex) + "-checkbox-" + (i + 1) + "'>" + quizContent[item.value][i] + "</label> \
 				                </div>"
-                }
-                this.container.querySelector('.quiz-step__checkbox-list').innerHTML = content;
-                this.directionTitle = item.value;
-            }
-        });
+				}
+				this.container.querySelector('.quiz-step__checkbox-list').innerHTML = content;
+				this.directionTitle = item.value;
+			}
+		});
 
-        this.container.querySelectorAll('.quiz__checkbox').forEach(item => {
-            item.addEventListener("click", ()=>{
-                this.container.querySelector('.quiz__btns-next').classList.add('disable'); 
-                
-                this.container.querySelectorAll('.quiz__checkbox').forEach(item => {
-                    if(item.querySelector('input').checked){
-                        this.container.querySelector('.quiz__btns-next').classList.remove('disable'); 
-                    }
-                })
-            });
-        });
+		this.container.querySelectorAll('.quiz__checkbox').forEach(item => {
+			item.addEventListener("click", () => {
+				this.container.querySelector('.quiz__btns-next').classList.add('disable');
 
-       
-    }
-    preparePrevСontent(){
-        this.container.querySelector('.quiz-step__btn').classList.add('add-none');
+				this.container.querySelectorAll('.quiz__checkbox').forEach(item => {
+					if (item.querySelector('input').checked) {
+						this.container.querySelector('.quiz__btns-next').classList.remove('disable');
+					}
+				})
+			});
+		});
 
-        let last = Object.keys(this.selectedQuiz)[Object.keys(this.selectedQuiz).length-1];
 
-        this.container.querySelectorAll(".quiz__radio-input").forEach(item => {
-            if(item.checked){
-                let content = '';
-                for(let i = 0; i < quizContent[last].length; i++){
-                    content += "<div class='quiz__checkbox'> \
-					                <input type='checkbox' id='quiz"+ (this.quizIndex) +"-checkbox-"+(i+1)+"'> \
-					                <label for='quiz"+ (this.quizIndex) +"-checkbox-"+(i+1)+"'>"+quizContent[last][i]+"</label> \
+	}
+	preparePrevСontent() {
+		this.container.querySelector('.quiz-step__btn').classList.add('add-none');
+
+		let last = Object.keys(this.selectedQuiz)[Object.keys(this.selectedQuiz).length - 1];
+
+		this.container.querySelectorAll(".quiz__radio-input").forEach(item => {
+			if (item.checked) {
+				let content = '';
+				for (let i = 0; i < quizContent[last].length; i++) {
+					content += "<div class='quiz__checkbox'> \
+					                <input type='checkbox' id='quiz"+ (this.quizIndex) + "-checkbox-" + (i + 1) + "'> \
+					                <label for='quiz"+ (this.quizIndex) + "-checkbox-" + (i + 1) + "'>" + quizContent[last][i] + "</label> \
 				                </div>"
-                }
-                this.container.querySelector('.quiz-step__checkbox-list').innerHTML = content;
-                this.directionTitle = item.value;
-            }
-        });
+				}
+				this.container.querySelector('.quiz-step__checkbox-list').innerHTML = content;
+				this.directionTitle = item.value;
+			}
+		});
 
-        this.container.querySelectorAll('.quiz__checkbox').forEach(item => {
-            item.addEventListener("click", ()=>{
-                this.container.querySelector('.quiz__btns-next').classList.add('disable'); 
-                
-                this.container.querySelectorAll('.quiz__checkbox').forEach(item => {
-                    if(item.querySelector('input').checked){
-                        this.container.querySelector('.quiz__btns-next').classList.remove('disable'); 
-                    }
-                })
-            });
-        });
-        
+		this.container.querySelectorAll('.quiz__checkbox').forEach(item => {
+			item.addEventListener("click", () => {
+				this.container.querySelector('.quiz__btns-next').classList.add('disable');
+
+				this.container.querySelectorAll('.quiz__checkbox').forEach(item => {
+					if (item.querySelector('input').checked) {
+						this.container.querySelector('.quiz__btns-next').classList.remove('disable');
+					}
+				})
+			});
+		});
 
 
-       
-    }
+
+
+	}
 
 
 }
@@ -243,5 +244,5 @@ class Quiz{
 
 let quiz = document.querySelectorAll(".quiz");
 quiz.forEach((item, i) => {
-    new Quiz(item, i);
+	new Quiz(item, i);
 })
